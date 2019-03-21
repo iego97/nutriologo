cf8525275fef83d6eaf69a6bee022d0b7c907332
Alumno <ulsa@iMacD101.local>
2019-03-20 18:42:19 -0700

    nutriologo

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+@section('titulo_pagina','Nutriologo | Sección')
+@section('titulo','Nutriólogo')
+@section('subtitulo','Sección')
+@section('contenido')
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Sección</h3>
+            </div>
+            <div class="box-body">
+                El contenido de mi dashboard
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
